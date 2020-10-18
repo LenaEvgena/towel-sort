3dbcce3aff1b86949334f9576ac248ebd233416b
Alena Astravukh <helen151204@gmail.com>
2020-10-18 15:16:17 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+// module.exports = function towelSort (matrix) {
+//   return [];
+
+    const towelSort = matrix.reduce((acc, cur) => {
+        if (cur % 2 === 0 || cur === 0) {
+            acc += cur;
+        } else {
+            if (cur % 2 !== 0) {
+                acc += cur.reverse();
+            }
+        }
+        return acc;
+    }
+
+    );
+
+
+
